099afbae425258a3a56ec3bb709f453580a8d6ff
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-05-10 12:33:23 +0530

    fix: copyright year in footer

--- static/js/main.js
+++ static/js/main.js
@@ -89,6 +89,22 @@ function send_email(recipient_email, subject, sender_email, message, redirect = 
         if (redirect){
             window.location.replace(redirect);
         }
-    }); 
+    });
 }
 
+function displayCurrentYear() {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+
+    // Find the span element dynamic-year
+    const yearSpan = document.querySelector('.dynamic-year');
+
+    // Update the content of the span with the current year
+    yearSpan.textContent = currentYear;
+}
+
+// Add an event listener for the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Call your function once the DOM is loaded
+    displayCurrentYear();
+});
